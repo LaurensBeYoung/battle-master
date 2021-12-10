99af33ca2f63cc7d6250c20fe86b7a28e27a3c65
KennyS <40846138+zhs1397@users.noreply.github.com>
2021-12-09 19:18:58 -0800

    Update Character.hs

--- src/Character.hs
+++ src/Character.hs
@@ -9,9 +9,17 @@ module Character
   , myheight, mywidth
   , player1,player2 --d, gameOver, --win, --rock, --unwalkable
   , score1,score2
-  , win1,win2,gameover
-  , forest
-  , hotdog
+  , win1,win2,gameOver
+  , forest,
+    hotdog1,
+    hotdog2,
+    hotdog3,
+    hotdog4,
+    hotdog5,
+    -- hotdog6,
+    beef,
+    beefs
+    --d, gameOver, --win, --rock, --unwalkable
   ) where
 
 import Control.Applicative ((<|>))
@@ -19,6 +27,7 @@ import Control.Monad (guard)
 import Data.Maybe (fromMaybe)
 
 import Hotdog
+
 
 import Control.Lens hiding ((<|), (|>), (:>), (:<))
 import Control.Monad.Trans.Maybe
@@ -29,7 +38,11 @@ import qualified Data.Sequence as S
 import Linear.V2 (V2(..), _x, _y)
 import System.Random (Random(..), newStdGen, getStdRandom)
 import System.IO.Unsafe
+import Hotdog
 
+myheight, mywidth :: Int
+myheight = 20
+mywidth  = 20
 type Coord = V2 Int
 
 --data Player = player1 | player2
@@ -42,10 +55,17 @@ data Game = Game
   , _score2 :: Int        
   , _win1 :: Bool            
   , _win2 :: Bool
-  , _gameover :: Bool
-  , _forest :: [Coord]
-  , _hotdog :: Hotdog
-  --, _woods :: [Coord]
+  , _forest :: [Coord]--, _woods :: [Coord]
+  , _hotdog1 :: Hotdog
+  , _hotdog2 :: Hotdog
+  , _hotdog3 :: Hotdog
+  , _hotdog4 :: Hotdog
+  , _hotdog5 :: Hotdog
+  -- , _hotdog6 :: Hotdog
+  , _beef :: Coord
+  , _beefs :: Stream Coord      -- ^ the location of the player will be modified via I/O
+  , _gameOver :: Bool            -- ^ the bool value mark the game is live or dead      
+  --, _rock :: [Coord]
   --, _unwalkable :: [Coord]
   } 
 
@@ -57,91 +77,115 @@ data Direction
   | West
   deriving (Eq, Show)
 
+data Stream a = Cons a (Stream a) deriving (Show,Eq, Ord)
+infixr 5 `Cons`
+infixr 5 <:>
+(<:>) :: a -> Stream a -> Stream a
+(<:>) = Cons
+
+head :: Stream a -> a
+head (Cons x _ ) = x
+
+tail :: Stream a -> Stream a
+tail (Cons _ xs) = xs
+
 
 makeLenses '' Game
 
 
-myheight, mywidth :: Int
-myheight = 20
-mywidth  = 20
-
+fromList :: [a] -> Stream a
+fromList = foldr Cons (error "Streams must be infinite")
 
 trees :: [Coord]
 trees = [(V2 0 11), (V2 0 12), (V2 1 12), (V2 1 11),(V2 2 11), (V2 2 10), (V2 3 10), (V2 3 11), (V2 4 11),              (V2 5 11), (V2 5 12),(V2 6 12), (V2 6 13),(V2 7 13),
-        (V2 7 12), (V2 8 12), (V2 8 11), (V2 9 11), (V2 9 12), (V2 10 12), (V2 10 11), (V2 11 11), (V2 11 12), (V2 12 12), (V2 12 13),(V2 13 13), (V2 13 12),(V2 14 12),(V2 14 11),(V2 15 11),
+        (V2 7 12), (V2 8 12), (V2 8 11), 
+        --(V2 9 11), (V2 9 12), (V2 10 12), (V2 10 11), 
+        (V2 11 11), (V2 11 12), (V2 12 12), (V2 12 13),(V2 13 13), (V2 13 12),(V2 14 12),(V2 14 11),(V2 15 11),
         (V2 16 11), (V2 17 11), (V2 18 11), (V2 18 12),(V2 19 12), (V2 20 12)
         ]
         
-  
+notInTree :: Coord -> Stream Coord -> [Coord] -> Coord
+notInTree s _ [] = s
+notInTree s d t = if s `elem` t then notInTree (Character.head d) (Character.tail d) t
+                  else s
+
+
 
 initGame :: IO Game
 initGame = do
+  -- (f :| fs) <-
+  --   fromList . randomRs (V2 0 0, V2 (mywidth - 1) (myheight - 10)) <$> newStdGen
+  (Cons f fs) <-
+    fromList . randomRs (V2 0 0, V2 (mywidth - 1) (myheight - 1)) <$> newStdGen
   let x1 = (mywidth `div` 2) - 1
       y1 = (myheight `div` 3) - 1
       x2 = (mywidth `div` 2) + 1
       y2 = (myheight `div` 3) - 1
+      fd = notInTree f fs trees 
       g = Game
         {
           _d = South
         , _player1 = (V2 x1 y1)
         , _player2 = (V2 x2 y2)
-        , _score1  = 10
-        , _score2  = 10
+        , _score1  = 5
+        , _score2  = 5
         , _win1 = False
         , _win2 = False
-        , _gameover = False
         , _forest   = trees
-        , _hotdog = initf
+        , _hotdog1 = initf1
+        , _hotdog2 = initf2
+        , _hotdog3 = initf3
+        , _hotdog4 = initf4
+        , _hotdog5 = initf5
+        -- , _hotdog6 = initf6
+        , _beef = fd
+        , _beefs = fs
+        , _gameOver = False
         }
   return (execState initState g)
 
+-- getLoc :: Coord
+-- getLoc = do 
+--   if newLoc `elem` trees then getLoc
+--   else newLoc
+--     where newLoc = randomRs (V2 0 0, V2 (mywidth - 1) (myheight - 1))
+
+-- initBeef ::  Game -> Game 
+-- initBeef g = g & beef %~ (\_ -> getLoc)
+
+
+
+getLoc :: Stream Coord -> [Coord] -> Game -> Coord
+getLoc d t g = if (Character.head d) `elem` t then getLoc (Character.tail d) t g
+             else if (Character.head d) == g^. beef then getLoc (Character.tail d) t g 
+             else Character.head d
+
+updateLoc :: Game -> Game
+updateLoc g = do
+  (g & beef %~ (\_ -> (getLoc (g ^. beefs) trees g))) & beefs %~ (\_ -> Character.tail (g ^. beefs))
+
 initState :: State Game ()
 initState = do
+  (Cons f fs) <- use beefs
+  beefs .= fs
   s <- get
   put s
 
--- check if play1 on border
-outBorder1:: Game -> Direction ->  Bool
-outBorder1 g North  = do
-  let (V2 x y) = g ^. player1
-  if y+1>=myheight then True
-  else False
-outBorder1 g South  = do
-  let (V2 x y) = g ^. player1
-  if y-1<=0 then True
-  else False
-outBorder1 g East  = do
-  let (V2 x y) = g ^. player1
-  if x+1>=mywidth then True
-  else False
-outBorder1 g West  = do
-  let (V2 x y) = g ^. player1
-  if x-1<=0 then True
-  else False
+checkWin1 :: Game -> Game
+checkWin1 g = do 
+  if g ^. score1>=10 then g & win1 %~ (\_ -> True)
+  else g
 
-  -- check if play2 on border
-outBorder2:: Game -> Direction ->  Bool
-outBorder2 g North  = do
-  let (V2 x y) = g ^. player2
-  if y+1>=myheight then True
-  else False
-outBorder2 g South  = do
-  let (V2 x y) = g ^. player2
-  if y-1<=0 then True
-  else False
-outBorder2 g East  = do
-  let (V2 x y) = g ^. player2
-  if x+1>=mywidth then True
-  else False
-outBorder2 g West  = do
-  let (V2 x y) = g ^. player2
-  if x-1<=0 then True
-  else False
+checkWin2 :: Game -> Game
+checkWin2 g = do 
+  if g ^. score2>=10 then g & win2 %~ (\_ -> True)
+  else g
+
 
 check :: Game -> Game
 check g = do
-  if g^. win1 == True then g & gameover %~ (\_ -> True)
-  else if g^. win2 == True then g & gameover %~ (\_ -> True)
+  if g^. win1 == True then g & gameOver %~ (\_ -> True)
+  else if g^. win2 == True then g & gameOver %~ (\_ -> True)
   else g
 
 check_die :: Game -> Game
@@ -159,7 +203,11 @@ moves1 North g = do
   let (V2 m n )= g ^. player2
   if x==m && y+1==n then g
   else if g ^. player1 `elem` trees then g & win2 %~ (\_ -> True)
-  else if g ^. gameover == True then g
+  else if g ^. score1 == 0 then g & win2 %~ (\_ -> True)
+  else if g ^. score2 == 0 then g & win1 %~ (\_ -> True)
+  -- check if have taken the beef
+  else if g ^. player1 == g ^. beef then  checkWin1 (updateLoc (g & score1 %~ (\x -> x+1))) 
+  else if g ^. gameOver == True then g
   else if y >= myheight-1 then g
   else 
     check(check_die (g)) & (player1 %~ (\(V2 a b) -> (V2 a (b+1))))
@@ -169,7 +217,11 @@ moves1 East g = do
   let (V2 m n )= g ^. player2
   if y==n && x+1==m then g
   else if g ^. player1 `elem` trees then g & win2 %~ (\_ -> True)
-  else if g ^. gameover == True then g
+  else if g ^. score1 == 0 then g & win2 %~ (\_ -> True)
+  else if g ^. score2 == 0 then g & win1 %~ (\_ -> True)
+  -- check if have taken the beef
+  else if g ^. player1 == g ^. beef then  checkWin1 (updateLoc (g & score1 %~ (\x -> x+1)))
+  else if g ^. gameOver == True then g
   else if x >= mywidth-1  then g
   else 
     check(check_die (g)) & (player1 %~ (\(V2 a b) -> (V2 (a+1) b)))
@@ -179,7 +231,11 @@ moves1 West g = do
   let (V2 m n )= g ^. player2
   if x==m+1 && y==n then g
   else if g ^. player1 `elem` trees then g & win2 %~ (\_ -> True)
-  else if g ^. gameover == True then g
+  else if g ^. score1 == 0 then g & win2 %~ (\_ -> True)
+  else if g ^. score2 == 0 then g & win1 %~ (\_ -> True)
+  -- check if have taken the beef
+  else if g ^. player1 == g ^. beef then  checkWin1 (updateLoc (g & score1 %~ (\x -> x+1)))
+  else if g ^. gameOver == True then g
   else if x <= 0 then g
   else 
     check(check_die (g)) & (player1 %~ (\(V2 a b) -> (V2 (a-1) b)))
@@ -189,7 +245,11 @@ moves1 South g = do
   let (V2 m n )= g ^. player2
   if x==m && y==n+1 then g
   else if g ^. player1 `elem` trees then g & win2 %~ (\_ -> True)
-  else if g ^. gameover == True then g
+  else if g ^. score1 == 0 then g & win2 %~ (\_ -> True)
+  else if g ^. score2 == 0 then g & win1 %~ (\_ -> True)
+  -- check if have taken the beef
+  else if g ^. player1 == g ^. beef then  checkWin1 (updateLoc (g & score1 %~ (\x -> x+1)))
+  else if g ^. gameOver == True then g
   else if y <= 0 then g
   else 
     check(check_die (g)) & (player1 %~ (\(V2 a b) -> (V2 a (b-1))))
@@ -202,7 +262,11 @@ moves2 North g = do
   let (V2 m n )= g ^. player1
   if x==m && y+1==n then g
   else if g ^. player2 `elem` trees then g & win1 %~ (\_ -> True)
-  else if g ^. gameover == True then g
+  else if g ^. score1 == 0 then g & win2 %~ (\_ -> True)
+  else if g ^. score2 == 0 then g & win1 %~ (\_ -> True)
+  -- check if have taken the beef
+  else if g ^. player2 == g ^. beef then  checkWin2 (updateLoc (g & score2 %~ (\x -> x+1)))
+  else if g ^. gameOver == True then g
   else if y >= myheight-1 then g
   else 
     check(check_die (g)) & (player2 %~ (\(V2 a b) -> (V2 a (b+1))))
@@ -212,7 +276,11 @@ moves2 East g = do
   let (V2 m n )= g ^. player1
   if y==n && x+1==m then g
   else if g ^. player2 `elem` trees then g & win1 %~ (\_ -> True)
-  else if g ^. gameover == True then g
+  else if g ^. score1 == 0 then g & win2 %~ (\_ -> True)
+  else if g ^. score2 == 0 then g & win1 %~ (\_ -> True)
+  -- check if have taken the beef
+  else if g ^. player2 == g ^. beef then  checkWin2 (updateLoc (g & score2 %~ (\x -> x+1)))
+  else if g ^. gameOver == True then g
   else if x >= mywidth-1  then g
   else 
     check(check_die (g)) & (player2 %~ (\(V2 a b) -> (V2 (a+1) b)))
@@ -222,7 +290,11 @@ moves2 West g = do
   let (V2 m n )= g ^. player1
   if x==m+1 && y==n then g
   else if g ^. player2 `elem` trees then g & win1 %~ (\_ -> True)
-  else if g ^. gameover == True then g
+  else if g ^. score1 == 0 then g & win2 %~ (\_ -> True)
+  else if g ^. score2 == 0 then g & win1 %~ (\_ -> True)
+  -- check if have taken the beef
+  else if g ^. player2 == g ^. beef then  checkWin2 (updateLoc (g & score2 %~ (\x -> x+1)))
+  else if g ^. gameOver == True then g
   else if x <= 0 then g
   else 
     check(check_die (g)) & (player2 %~ (\(V2 a b) -> (V2 (a-1) b)))
@@ -232,11 +304,12 @@ moves2 South g = do
   let (V2 m n )= g ^. player1
   if x==m && y==n+1 then g
   else if g ^. player2 `elem` trees then g & win1 %~ (\_ -> True)
-  else if g ^. gameover == True then g
+  else if g ^. score1 == 0 then g & win2 %~ (\_ -> True)
+  else if g ^. score2 == 0 then g & win1 %~ (\_ -> True)
+  -- check if have taken the beef
+  else if g ^. player2 == g ^. beef then checkWin2 (updateLoc (g & score2 %~ (\x -> x+1))) 
+  else if g ^. gameOver == True then g
   else if y <= 0  then g
   else 
     check(check_die (g)) & (player2 %~ (\(V2 a b) -> (V2 a (b-1))))
-
-
-
 
